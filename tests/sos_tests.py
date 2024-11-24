sosreport/sos

[pylint] Fixes more pylint issues
The following are fixed in this change:

    W3101 - missing-timeout
    W0231 - super-init-not-called
    C0123 - unidiomatic-typecheck
    C0207 - use-maxsplit-arg
    E0611 - no-name-in-module
    E0702 - raising-bad-type
    E1101 - no-member
    E1135 - unsupported-membership-test
    C0411 - wrong-import-order

Signed-off-by: Ponnuvel Palaniyappan <pponnuvel@gmail.com>

tests/sos_tests.py
@@ -389,7 +389,7 @@ class BaseSoSReportTest(BaseSoSTest):
                 self._manifest = json.loads(content)
             except Exception:
                 self._manifest = ''
-                self.warning('Could not load manifest for test')
+                self.log.debug('Could not load manifest for test')
         return self._manifest
 
     @property

NVIDIA/doca-sosreport

tests/sos_tests.py
@@ -389,7 +389,7 @@ class BaseSoSReportTest(BaseSoSTest):
                 self._manifest = json.loads(content)
             except Exception:
                 self._manifest = ''
-                self.warning('Could not load manifest for test')
+                self.log.warn('Could not load manifest for test')
         return self._manifest
 
     @property